f5e3d0511d18a1041a246950d526408bbc659a05
Stelian Grosu <stelian.email@yahoo.com>
2021-01-27 12:09:30 -0500

    stelian movies

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,73 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
-// Iteration 3: All rates average - Get the average of all rates with 2 decimals
-
-// Iteration 4: Drama movies - Get the average of Drama Movies
-
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-
-// BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-
-// BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function getAllDirectors(dirArr) {
+    return dirArr.map((movie) => {
+      return movie.director;
+    });
+  }
+  
+  
+  
+  function howManyMovies(array) {
+    return array.filter((movie) => {
+      const directedStevenS = movie.director == "Steven Spielberg";
+      const dramaMovie = movie.genre.includes("Drama");
+      return directedStevenS && dramaMovie;
+    }).length;
+  }
+  
+ 
+  function ratesAverage(myArr) {
+    return (
+      Number(
+        (
+          myArr.reduce((acc, movie) => {
+            return (acc += movie.rate || 0);
+          }, 0) / myArr.length
+        ).toFixed(2)
+      ) || 0
+    );
+  }
+  
+  
+  function dramaMoviesRate(array) {
+    let filteredDramaMovies = array.filter((eachMovie) => {
+      return eachMovie.genre.includes('Drama')
+    })
+  
+    return ratesAverage(filteredDramaMovies)
+  }
+  
+  
+  function orderByYear(array) {
+    console.log(array)
+    const result = array.sort((a, b) => {
+      if (a.year > b.year) {
+        return 1;
+      } else if (b.year > a.year) {
+        return -1;
+      } else {
+  
+        return a.title.localeCompare(b.title);
+      }
+    })
+    return [...result];
+  }
+  
+  
+  
+  
+  
+  function orderAlphabetically(arr) {
+  
+    let sortedArray = [...arr].sort((a, b) => {
+      return (a.title.localeCompare(b.title)) //it is either 0,1,-1
+  
+  
+    })
+  
+    let first20titles = sortedArray.slice(0, 20).map(item => {
+      return item.title
+    })
+  
+    return first20titles
+    
